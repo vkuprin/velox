429cf1be0893cd8f4bb239f56f5b02d3413a5d08
askachkou <askachkou@joblio.co>
2022-09-07 15:12:20 +0300

    JWT

---
+++ server/src/main/java/com/velox/server/repository/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package com.velox.server.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import com.velox.server.models.RefreshToken;
+import com.velox.server.models.User;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+  Optional<RefreshToken> findByToken(String token);
+
+  @Modifying
+  int deleteByUser(User user);
+}
