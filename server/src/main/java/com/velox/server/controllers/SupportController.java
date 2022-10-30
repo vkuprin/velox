ba2f118ed94b44cf994d7b55322edb1ab570573e
askachkou <askachkou@joblio.co>
2022-10-30 21:01:13 +0200

    cors

--- server/src/main/java/com/velox/server/controllers/SupportController.java
+++ server/src/main/java/com/velox/server/controllers/SupportController.java
@@ -3,12 +3,14 @@ package com.velox.server.controllers;
 import com.velox.server.models.Role;
 import com.velox.server.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/dict")
 public class SupportController {
